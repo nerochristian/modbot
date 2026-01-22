6cef2e40860468d5860fff34fce78a8ef2b3db50
Nero Christian <nero.christian@campioncollege.com>
2026-01-21 22:16:19 -0500

    Update gelbooru_loop.py

--- cogs/gelbooru_loop.py
+++ cogs/gelbooru_loop.py
@@ -12,7 +12,7 @@ class GelbooruLoop(commands.Cog):
         self.channel_id = 1463001859345354968
         
         # Tags: 'sort:random' prevents repeats. 'rating:general' keeps it safe.
-        self.tags = "femboy animated rating:general sort:random"
+        self.tags = "femboy rating:general sort:random"
         
         self.api_key = "4b5d1fd9db037eeb8b534b57f7d3d5e7f58f8ad8d3045fb75bd4f11f3db95345bef64f2551fd74a43b62b3f544996df06b01fec2cbb4b4cae335168f207855f2"
         self.user_id = "1900224"
